c7197b9fb9f29cd89f163c73976a1f3dd28a124c
Brady Pascoe <bpas247@hotmail.com>
2018-04-12 11:39:29 -0700

    Added Bot Utilities that includes helper functions

---
+++ src/main/java/BotUtils.java
@@ -0,0 +1,34 @@
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.RequestBuffer;
+
+public class BotUtils {
+    // Constants for use throughout the bot
+    static String BOT_PREFIX = "__";
+
+    // Handles the creation and getting of a IDiscordClient object for a token
+    static IDiscordClient getBuiltDiscordClient(String token) {
+
+        // The ClientBuilder object is where you will attach your params for configuring the instance of your bot.
+        // Such as withToken, build, etc.
+        return new ClientBuilder().withToken(token).build();
+
+    }
+
+    // Helper functions to make certain aspects of the bot easier to use.
+    static void sendMessage(IChannel channel, String message) {
+
+        //Requests to send the message to the discord channel.
+        //Will throw an exception if it cannot send the mssage
+        RequestBuffer.request(() -> {
+            try{
+                channel.sendMessage(message);
+            } catch (DiscordException e){
+                System.err.println("Message could not be sent with error: ");
+                e.printStackTrace();
+            }
+        });
+    }
+}
